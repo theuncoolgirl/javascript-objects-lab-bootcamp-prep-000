11f84cb55109e5f5874018c6a3ed29fca5e1477a
Erin Shields <elshields92@gmail.com>
2020-04-18 14:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const recipes = { breakfast: "eggs", lunch: "sandwich", dinner: "lasagna"};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
