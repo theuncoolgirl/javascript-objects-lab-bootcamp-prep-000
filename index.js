ed8eddd21ee949c29a67f294817f7dbea557a772
Erin Shields <elshields92@gmail.com>
2020-04-18 14:49:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  return object
 }
